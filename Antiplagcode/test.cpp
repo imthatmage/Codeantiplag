c473ec5fbf75f79809dc56b5c75d07a69dce8043
Almir <vendigo1632@gmail.com>
2020-03-07 22:50:29 +0300

    Second part of modifying suppose to be ready soon

--- Antiplagcode/test.cpp
+++ Antiplagcode/test.cpp
@@ -7,9 +7,13 @@ static std::string m_str = "";
 
 int main()
 {
+	//C:\\Users\\Hindgarden\\source\\repos\\antiplag\\Antiplagcode\\hello.txt
+	std::cout << "Give exact path to file to check";
+	std::string tmp;
+	std::cin >> tmp;
 	std::regex reg("(?://.*?\n)|(?:/\\*(?:.|\n)*?\\*/)");
 
-	std::ifstream in("C:\\Users\\Hindgarden\\source\\repos\\antiplag\\Antiplagcode\\hello.txt");
+	std::ifstream in(tmp);
 	std::cout << in.is_open() << "open" << std::endl;
 	if (in.is_open())
 	{
@@ -19,5 +23,6 @@ int main()
 			m_str += (tmp + '\n');
 		}
 	}
-	std::cout<< std::regex_replace(m_str, reg, "");
+	m_str = std::regex_replace(m_str, reg, "");
+	reg = "\\s|=|+|-|*|/|%|>|<|(?:+=)|(?:-=)|(?:*=)|(?:/=)|(?:%=)|(?:==)|(?:!=)|(?:>=)|(?:<=)";
 }
