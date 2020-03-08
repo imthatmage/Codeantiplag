79ab5af1c5f71e02a368f50a77ef65d56a269f1b
Almir <vendigo1632@gmail.com>
2020-03-08 22:51:54 +0300

    At the main file added regex that bring whitespace to order

---
+++ Antiplagcode/ven.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+static std::string m_str = "";
+
+int main()
+{
+	//C:\\Users\\Hindgarden\\source\\repos\\antiplag\\Antiplagcode\\hello.txt
+	std::cout << "Give exact path to file to check";
+	std::string tmp;
+	std::cin >> tmp;
+	std::regex reg("(?://.*?\n)|(?:/\\*(?:.|\n)*?\\*/)|(?:#.*)");
+
+	std::ifstream in(tmp);
+	std::cout << in.is_open() << "open" << std::endl;
+	if (in.is_open())
+	{
+		std::string tmp;
+		while (std::getline(in, tmp))
+		{
+			m_str += (tmp + '\n');
+		}
+	}
+	m_str = std::regex_replace(m_str, reg, "");
+	reg = "(?:\\[\\])|(?:\\:\\:)|(?:\\-\\>)|(?:\\<\\<)|(?:\\>\\>)|(?:\\+\\=)|(?:\\-\\=)|(?:\\*\\=)|(?:\\/\\=)|(?:\\%\\=)|(?:\\&\\=)|(?:\\^\\=)|(?:\\<\\<\\=)|(?:\\>\\>=)|(?:\\+\\+)|(?:\\-\\-)|(?:\\=\\=)|(?:\\!\\=)|(?:\\>\\=)|(?:\\<\\=)|!|(?:\\&\\&)|(?:\\|\\|)|\\=|\\+|\\-|\\*|\\/|\\%|\\~|\\&|\\>|\\<|\\^|(?:\\.)|(?:\\,)|(?:\\:)|(?:\\;)|(?:\\[)|(?:\\])|(?:\\{)|(?:\\})|(?:\\()|(?:\\))|(?:\\:)";
+	m_str = std::regex_replace(m_str, reg, " $& ");
+	reg = "(\\s)+";
+	m_str = std::regex_replace(m_str, reg, " ");
+	std::cout << m_str;
+}
