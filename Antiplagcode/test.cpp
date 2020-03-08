dc62fc5acbddfcdf0461c6bd3e9bf7817f26c087
Almir <vendigo1632@gmail.com>
2020-03-08 22:48:04 +0300

    Added testing cpp file

--- Antiplagcode/test.cpp
+++ Antiplagcode/test.cpp
@@ -2,27 +2,29 @@
 #include <fstream>
 #include <string>
 #include <regex>
-
-static std::string m_str = "";
+//examples
+static std :: string m_str;
+void func (   int a    , int b   )
+{
+    std::cout    <<      "Fafa";
+    int mass     [3] = {1, 2, 3};
+    std::cout << mass       [   0     ];
+    std::string tmp0="331";
+    std::string tmp1   ="331";
+    int aa = 3;
+    int bb=      45;
+    int     c  = b *   a +    4;
+    c   *= c  = b *   a +    4;
+    ++       aa;
+    int massd[1][1] = {{2}};
+    int g = massd[0]     [0];
+}
 
 int main()
 {
-	//C:\\Users\\Hindgarden\\source\\repos\\antiplag\\Antiplagcode\\hello.txt
-	std::cout << "Give exact path to file to check";
-	std::string tmp;
-	std::cin >> tmp;
-	std::regex reg("(?://.*?\n)|(?:/\\*(?:.|\n)*?\\*/)");
-
-	std::ifstream in(tmp);
-	std::cout << in.is_open() << "open" << std::endl;
-	if (in.is_open())
-	{
-		std::string tmp;
-		while (std::getline(in, tmp))
-		{
-			m_str += (tmp + '\n');
-		}
-	}
-	m_str = std::regex_replace(m_str, reg, "");
-	reg = "\\s|=|+|-|*|/|%|>|<|(?:+=)|(?:-=)|(?:*=)|(?:/=)|(?:%=)|(?:==)|(?:!=)|(?:>=)|(?:<=)";
+    func(3, 2);
+    std::string tmp = "1^ 2+= 3+ 4- 5&& 6>> 7< 8| 9( 10* 11: 12; 13. 14, 15) 16{ 17[ 18] 19:: 20-> 21> 22! 23-";
+    std::regex r("\\-|(?:\\+\\=)|(?:\\-\\>)|(?:\\+)|(?:\\&\\&)|(?:\\>\\>)|(?:\\<)|(?:\\-)|(?:\\|)|(?:\\()|(?:\\*)|(?:\\^)|(?:\\:)|(?:\\;)|(?:\\.)|(?:\\,)|(?:\\[)|(?:\\])|(?:\\{)|(?:\\})|(?:\\()|(?:\\))|\\>|!");
+    
+    std::cout << std::regex_replace(tmp, r, "[$&]");
 }
