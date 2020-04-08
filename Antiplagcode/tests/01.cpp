8b57bf105cbf203170ad86c301a15f74f1e79532
Almir <nurm.almir@gmail.com>
2020-04-08 23:31:13 +0300

    3 test files added

---
+++ Antiplagcode/tests/01.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <cstddef>
+#include <string>
+
+class SETTER
+{
+public:
+	SETTER(size_t S)
+	{
+		setter = new std::set<long long>[S + 1];
+	}
+	void add(size_t index, long long elem)
+	{
+		auto tmp = mapper.find(elem);
+		if (tmp != mapper.end())
+		{
+			(*tmp).second.insert(index);
+			setter[index].insert(elem);
+		}
+		else
+		{
+			std::set<int> jj;
+			jj.insert(index);
+			mapper.emplace(elem, jj);
+			setter[index].insert(elem);
+		}
+	}
+	void deleter(size_t index, long long elem)
+	{
+		auto& tmp = (*(mapper.find(elem))).second;
+		tmp.erase(tmp.find(index));
+		setter[index].erase(setter[index].find(elem));
+	}
+	void clear(size_t index)
+	{
+		for (const long long& tmp : setter[index])
+		{
+			auto jingo = mapper.find(tmp);
+			(*jingo).second.erase((*jingo).second.find(index));
+		}
+		setter[index].clear();
+	}
+	void listset(size_t index)
+	{
+		if (!(setter[index].empty()))
+		{
+			for (const long long& tmp : setter[index])
+			{
+				std::cout << tmp << ' ';
+			}
+			std::cout << std::endl;
+		}
+		else
+		{
+			std::cout << "-1" << std::endl;
+		}
+	}
+	void listsetof(long long elem)
+	{
+		auto itera = mapper.find(elem);
+		if (itera != mapper.end())
+		{
+			auto tmp = (*itera).second;
+			if (!(tmp.empty()))
+			{
+				for (int vivi : tmp)
+				{
+					std::cout << vivi << ' ';
+				}
+				std::cout << std::endl;
+			}
+			else
+			{
+				std::cout << "-1" << std::endl;
+			}
+		}
+		else
+		{
+			std::cout << "-1" << std::endl;
+		}
+	}
+private:
+	std::set<long long>* setter = nullptr;
+	std::map<long long, std::set<int>> mapper;
+	size_t size = 0;
+	long long max = 0;
+};
+
+int main()
+{
+	long long N;
+	size_t M;
+	size_t K;
+	std::cin >> N >> M >> K;
+	SETTER gorg(M);
+	std::string str;
+	long long num;
+	int setnum;
+	for (size_t i = 0; i < K; ++i)
+	{
+		std::cin >> str;
+		if (str == "ADD")
+		{
+			std::cin >> num >> setnum;
+			gorg.add(setnum, num);
+		}
+		else if (str == "DELETE")
+		{
+			std::cin >> num >> setnum;
+			gorg.deleter(setnum, num);
+		}
+		else if (str == "CLEAR")
+		{
+			std::cin >> setnum;
+			gorg.clear(setnum);
+		}
+		else if (str == "LISTSET")
+		{
+			std::cin >> setnum;
+			gorg.listset(setnum);
+		}
+		else if (str == "LISTSETSOF")
+		{
+			std::cin >> num;
+			gorg.listsetof(num);
+		}
+	}
+}
+
---
+++ Antiplagcode/tests/03.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <cstddef>
+#include <string>
+#include <vector>
+#include <list>
+
+//just renamed
+class SETTE
+{
+public:
+	SETTE(size_t Ss)
+	{
+		sette                        =                        new std::set<long long>[Ss + 1];
+	}
+	void            adds      (      size_t         index_, long long elem_)
+	{
+		auto tmp_ = mappe.find(elem_);
+		if (tmp_ != mappe.end())
+		{
+			(*tmp_).      second.insert(index_);
+			sette[index_].insert(elem_);
+		}
+		else
+		{
+			std::set        <        int> jj;
+			jj.insert(index_);
+			mappe          .emplace         (elem_, jj);
+			sette          [index_          ].insert(elem_);
+		}
+	}
+	void        deleters            (size_t index_, long long elem_)
+	{
+		auto& tmp_ = (*(mappe.find(elem_))).second;
+		tmp_.erase(tmp_.find(index_));
+		sette[index_].erase(sette[index_].find(elem_));
+	}
+	void         clears       (size_t       index_)
+	{
+		for (const long long&           tmp_ : sette[index_])
+		{
+			auto jingo_ = mappe.find(tmp_);
+			(*jingo_).second.erase((*jingo_).second.find(index_));
+		}
+		sette[index_].clear();
+	}
+	void listsets(        size_t index_)
+	{
+		if (!(sette[index_].empty()))
+		{
+			for (const long long& tmp_ : sette[index_])
+			{
+				std         ::cout << tmp_ << ' ';
+			}
+			std::        cout << std::endl;
+		}
+		else
+		{
+			std             ::      cout << "-1" << std::endl;
+		}
+	}
+	void listsetofs(long long elem_)
+	{
+		auto itera_=mappe.find(elem_);
+		if(itera_!=mappe.end())
+		{
+			auto tmp_=(*itera_).second;
+			if (!(tmp_.empty()))
+			{
+				for(int vivi_:tmp_)
+				{
+					std::cout<<vivi_<< ' ';
+				}
+				std::cout<<std::endl;
+			}else{
+				std::cout<<"-1"<< std::endl;
+			}
+		} else {
+			std::cout<<"-1"<<std::endl;
+		}
+	}
+private:
+	std::set<long long>* sette = nullptr;
+	std::map<long long,std::set<int>>mappe;
+	size_t size_ = 0;
+	long long max_ = 0;
+};
+
+int main(){
+	long long                                      N_;
+	size_t M_;
+	size_t K             ;
+	std::cin>> N_       >> M_    >>K;
+	SETTE gorg_(M_);
+	std::string str_;
+	long long num_;
+	int setnum_;
+	for (size_t i       = 0; i      < K; ++i)
+	{
+		std::cin >>         str_;
+		if (          str_ == "ADD")
+		{
+			std::cin        >> num_ >> setnum_;
+			gorg_.adds(        setnum_, num_);
+		}
+		else if (str_=="DELETE")
+		{
+			std::cin>>num_>>setnum_;
+			gorg_.deleters(setnum_,num_);
+		}
+		else if (str_=="CLEAR")
+		{
+			std::cin>>setnum_;
+			gorg_.clears(setnum_);
+		}
+		else if (str_=="LISTSET")
+		{
+			std::cin>>setnum_;
+			gorg_.listsets(setnum_);
+		}
+		else if (str_=="LISTSETSOF")
+		{
+			std::cin>>num_;
+			gorg_.listsetofs(num_);
+		}
+	}
+}
