5d721236c1cec4e55cef68f717962bd9b95544b8
Almir <nurm.almir@gmail.com>
2020-04-11 21:40:53 +0300

    Reinitial visual-qt-form and some initial widgets added

---
+++ Almplag/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_chooseButton_clicked()
+{
+    QString path = QFileDialog::getOpenFileName(0, "Choose file", "");
+    ui->linePath->setText(path);
+}
